57381b65ae3e9a5a31a732fa7fe4f3b19632c553
Ricardo Pablo De Leon <rjdeleon@up.edu.ph>
2020-02-08 18:20:20 +0800

    Replace coroutine flow project files

--- AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/model/NewsService.kt
+++
@@ -1,9 +0,0 @@
-package com.devtides.androidcoroutinesflow.model
-
-import com.devtides.androidcoroutinesretrofit.model.NewsArticle
-import retrofit2.http.GET
-
-interface NewsService {
-    @GET("news.json")
-    suspend fun getNews(): List<NewsArticle>
-}
--- AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
+++ AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -2,11 +2,9 @@ package com.devtides.androidcoroutinesflow.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.devtides.androidcoroutinesflow.model.NewsRepository
 
 class ListViewModel: ViewModel() {
 
-    val newsArticles = NewsRepository().getNewsArticles().asLiveData()
+    val newsArticles = MutableLiveData<String>()
 
 }
